8bad4348bca1614cac5f99faefc00e677aeb4dcf
inkxk_ <nokink@hotmail.com>
2022-04-10 17:48:49 +0700

    init GameList

---
+++ src/components/GameList/GameList.render.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react'
+import { Game } from 'types/GlobalTypes'
+
+type Props = {
+	err?: string
+	games: Game[]
+}
+
+const GameList = ({ err, games }: Props): ReactElement => {
+	return <></>
+}
+
+export default GameList
