f2d6d71458c69017e58625a983e338b306bbced0
inkxk_ <nokink@hotmail.com>
2022-04-17 23:02:02 +0700

    completed!

--- src/components/GameFilter/GameFilter.tsx
+++ src/components/GameFilter/GameFilter.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent, ReactElement } from 'react'
 import { GENRES, PLATFORMS, SORT_BY, TAGS } from './constants'
-import { Form } from './styles'
+import { Form, Label, Select } from './styles'
 
 type Props = {
 	onChange: (e: ChangeEvent<HTMLFormElement>) => void
@@ -9,49 +9,49 @@ type Props = {
 const GameFilter = ({ onChange }: Props): ReactElement => {
 	return (
 		<Form onChange={onChange}>
-			<label htmlFor="platform-select">
+			<Label htmlFor="platform-select">
 				Platform:
-				<select name="platform" id="platform-select">
+				<Select name="platform" id="platform-select">
 					{PLATFORMS.map(platform => (
 						<option key={platform.value} value={platform.value}>
 							{platform.display}
 						</option>
 					))}
-				</select>
-			</label>
+				</Select>
+			</Label>
 
-			<label htmlFor="genre-select">
+			<Label htmlFor="genre-select">
 				Genre:
-				<select name="genre" id="genre-select">
+				<Select name="genre" id="genre-select">
 					{GENRES.map(genre => (
 						<option key={genre.value} value={genre.value}>
 							{genre.display}
 						</option>
 					))}
-				</select>
-			</label>
+				</Select>
+			</Label>
 
-			<label htmlFor="tag-select">
+			<Label htmlFor="tag-select">
 				Tag:
-				<select name="tag" id="tag-select">
+				<Select name="tag" id="tag-select">
 					{TAGS.map(tag => (
 						<option key={tag.value} value={tag.value}>
 							{tag.display}
 						</option>
 					))}
-				</select>
-			</label>
+				</Select>
+			</Label>
 
-			<label htmlFor="sortBy-select">
+			<Label htmlFor="sortBy-select">
 				Sort By:
-				<select name="sortBy" id="sortBy-select">
+				<Select name="sortBy" id="sortBy-select">
 					{SORT_BY.map(sortBy => (
 						<option key={sortBy.value} value={sortBy.value}>
 							{sortBy.display}
 						</option>
 					))}
-				</select>
-			</label>
+				</Select>
+			</Label>
 		</Form>
 	)
 }
--- src/components/GameFilter/styles.tsx
+++ src/components/GameFilter/styles.tsx
@@ -1,5 +1,36 @@
 import styled from 'styled-components'
+import { breakpoints } from 'styles/breakpoints/breakpoints'
+import { backgroundColor, secondaryTextColor } from 'styles/theme/theme'
 
 export const Form = styled.form`
 	display: flex;
+	justify-content: space-between;
+	max-width: 542px;
+	padding: 0 16px;
+	margin: 24px auto;
+	color: ${secondaryTextColor};
+	box-sizing: border-box;
+
+	@media (min-width: ${breakpoints.tablet}) {
+		max-width: ${breakpoints.tablet};
+	}
+
+	@media (min-width: ${breakpoints.desktop}) {
+		max-width: 1010px;
+	}
+`
+export const Label = styled.label`
+	margin-right: 16px;
+	flex-basis: 25%;
+
+	&:last-of-type {
+		margin-right: 0;
+	}
+`
+
+export const Select = styled.select`
+	width: 100%;
+	margin-top: 0;
+	color: white;
+	background-color: ${backgroundColor};
 `
